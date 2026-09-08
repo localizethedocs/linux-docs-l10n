f1cac2dac1b6bbace19843a41b0d74fd85c25b49
ltdrobot <ltdrobot@localizethedocs.org>
2026-09-08 13:30:43 +0000

    Deploying to pages from @ localizethedocs/linux-docs-l10n@c7ca2d09ea77f3e0d9326bc0a040b69295da87c9 🚀

---
+++ ltd-config.js
@@ -0,0 +1,20 @@
+"use strict";
+
+var CONFIG_OPTIONS = {
+  CONFIG_LANGUAGES: [
+    ["en-us",   "English"],
+    ["zh-cn",   "简体中文"],
+    ["zh-tw",   "繁體中文"],
+  ],
+  CONFIG_VERSIONS: [
+    ["master",  "Development"],
+    ["6",       "Release 6"],
+  ],
+  CONFIG_PROJECTS: [
+    ["Index",   "https://projects.localizethedocs.org"],
+    ["Crowdin", "https://localizethedocs.crowdin.com/linux-docs-l10n"],
+    ["GitHub",  "https://github.com/localizethedocs/linux-docs-l10n"],
+    ["GitCode", "https://gitcode.com/localizethedocs/linux-docs-l10n"],
+    ["GitFlic", "https://gitflic.ru/project/localizethedocs/linux-docs-l10n"],
+  ]
+};
